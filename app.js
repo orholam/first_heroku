373559fc3b767be1650a9329e4c77a5a0c1ac557
orholam <josiaha@uchicago.edu>
2022-11-20 20:32:58 -0600

    stuffing

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const app = require("express")();
 
 const PORT = process.env.PORT || 3000;
 
+let out = "<html><body><h1>Hello World!</h1><p>This is my first Heroku app</p></body></html>";
+
 app.get("",(req, res) => {
-        res.send("Hello World!");
+        res.send(out);
 });
 
 app.listen(PORT, () => {
